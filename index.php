456115472d549c604b5743cb576f6efc775be7b7
mc-soft <matthew.croston@btinternet.com>
2019-07-27 18:35:29 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en-GB">
+    <head>
+        <title>How humble are you?</title>
+
+        <link rel="stylesheet" href="assets/css/main.css">
+    </head>
+
+    <body>
+
+        <footer>
+            <script type="text/javascript" src="assets/js/main.js"></script>
+        </footer>
+    </body>
+</html>
